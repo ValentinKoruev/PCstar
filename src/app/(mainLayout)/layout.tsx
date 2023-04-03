eed3b9615eb02a38b75d0ec64d9c1bc7ec5829c8
Dbeliq <doopster123@gmail.com>
2023-04-04 01:12:47 +0300

    feat: open sans next/font added to layout body (testing)

--- src/app/(mainLayout)/layout.tsx
+++ src/app/(mainLayout)/layout.tsx
@@ -1,6 +1,14 @@
 import '../globals.scss';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import { Open_Sans } from 'next/font/google';
+
+const openSans = Open_Sans({
+  weight: ['400', '700'],
+  subsets: ['cyrillic', 'latin'],
+  fallback: ['Helvetica', 'Arial', 'sans-serif']
+})
+
 
 export const metadata = {
   title: 'PC STORE',
@@ -14,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={openSans.className}>
         <Header isStatic={false} />
           {children}
         <Footer />
