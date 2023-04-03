629a48e4cd223c0198d747ca0c98d46a9a015808
Dbeliq <doopster123@gmail.com>
2023-04-03 09:38:35 +0300

    feat: partial prisma mongodb implementation

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,35 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../../server/db/client"
+
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+
+  const items = await prisma.item.findMany({ where: {
+    categories: {has: '/test/sub'}
+  } });
+
+
+  return NextResponse.json(items, {
+    status: 200,
+  })
+}
+
+
+export async function POST(request: Request) {
+
+  let {title, price, categories} = await request.json();
+
+  // let categoriesArray = JSON.parse(categories);
+  price = parseFloat(price);
+  // const item = await prisma.item.create({
+  //   data: {
+  //     title,
+  //     price,
+  //     categories
+  //   }
+  // })
+
+  return NextResponse.json(categories, {
+    status: 200,
+  })
 }
